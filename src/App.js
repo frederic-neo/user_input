7ed955e5b146a2125e53cba69117799cbcb84753
frederic-neo <frederic@neoito.com>
2022-11-03 16:13:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import User_input from './user_input'
+export default function App() {
+  return (
+    <div>
+      <User_input/>
+    </div>
+  )
+}
---
+++ src/user_input.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const User_input = () => {
+  return (
+   <p>Hello from user_input </p>
+  )
+}
+export default User_input
